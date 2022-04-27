3472717c1f331df09364b21712deffd5d4ea566e
Abhinav Anand <abhi9vanand@gmail.com>
2022-04-27 12:10:46 +0530

    files added

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import Navigation from './Navigation';
+
+const App = () => {
+  return (
+    <Navigation /> 
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
